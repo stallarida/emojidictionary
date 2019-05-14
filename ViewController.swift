49d39bc34a30a96c182fde92b7012d837f62290c
scottt <stallarida@gmail.com>
2019-05-14 17:43:33 -0500

    working thru tutorial

--- ViewController.swift
+++ ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    @IBOutlet weak var tableViewYo: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableViewYo.dataSource = self
+        tableViewYo.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Howdy Y'all 😡"
+        return cell
+        
     }
 
 
