ba8fcf2737651f4a47e22ee534a01c6733d5648e
scottt <stallarida@gmail.com>
2019-05-14 14:21:20 -0500

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Scott Tallarida on 5/14/19.
+//  Copyright © 2019 Scott Tallarida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
