f31e07a3a7182a97d5b98a77188132f7e2a7d3f9
scottt <stallarida@gmail.com>
2019-05-16 14:25:57 -0500

    added shit

--- ViewController.swift
+++ ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
 
     @IBOutlet weak var tableViewYo: UITableView!
+    
+    var emojis = ["🥶","🤯","😡","😜","👽","🐱"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +25,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Howdy Y'all 😡"
+        cell.textLabel?.text =  emojis[indexPath.row]
         return cell
         
     }
